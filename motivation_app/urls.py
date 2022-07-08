b94c1dfd501ae6488a5ff86a4cae16543c3a3832
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-07-08 13:52:33 +0300

    ft:deployment - deploy to heroku

--- motivation_app/urls.py
+++ motivation_app/urls.py
@@ -60,7 +60,12 @@ urlpatterns = [
     path('admin/post/<int:pk>/', views.SinglePostList.as_view(), name='singlepost'),
     path('admin/add_users/', views.AddUser.as_view(), name='singlepost'),
     path('admin/deactivate/', views.DeactivateUser.as_view(), name='deactivate'),
-    path('all_users/', views.RegisteredUsers.as_view(), name='all_users')
+    
+    #General apiendpoints
+    path('all_users/', views.RegisteredUsers.as_view(), name='all_users'),
+    path('all_comments/', views.commentsList.as_view(), name='comments'),
+    path('all_likes/', views.LikesList.as_view(), name='likes'),
+    
     
     
 ]
