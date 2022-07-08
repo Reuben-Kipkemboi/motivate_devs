b573402525c61f069a36848679aa9b2d4671af5a
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-07-08 17:23:07 +0300

    ft:staff-category creation - adds categories

--- motivation_project/settings.py
+++ motivation_project/settings.py
@@ -187,5 +187,12 @@ ACCOUNT_EMAIL_UNIQUE=True
 #     'https://polite-fairy-12efa3.netlify.app'
 #     'https://josephatngugi.github.io/Motivation-App-Frontend',
 # ]
-CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOW_ALL_ORIGINS = (
+    True  # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+)
 CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOWED_ORIGINS = []
+
+
